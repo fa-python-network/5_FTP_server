7a0ca950c35217f58788d31578ede2a6602e9980
Павлова Полина Сергеевна <181413@edu.fa.ru>
2019-11-08 09:56:13 +0300

    Функции чтения содержимого файла, создания/удаления директории, удаления файла, логирования, проверки адреса на доступность (для удаления/создания директории), подтверждения удаления

--- ftp-client.py
+++ ftp-client.py
@@ -1,13 +1,40 @@
 import socket
+import os
 
-HOST = 'localhost'
+HOST = "localhost"
 PORT = 6666
+userdir = os.path.join(os.getcwd(), "docs")
 
-while True:
-    request = input('>')
-    if request=='exit':
-        break
+def verify_delete(path):
+    deltype=tuple()
+    fullpath=str()
+    if os.path.isabs(path):
+        fullpath = path
+    else:
+        fullpath = os.path.join(userdir,path)
+    if os.path.isdir(fullpath):
+        deltype=("a", "папка")
+    else:
+        deltype=("", "файл")
+    print("Будет удален{} {} {}. Продолжить?".format(deltype[0],deltype[1],fullpath))
+    answer=input()
+    if str.lower(answer) in(["да","д","yes","y"]):
+        return True
+    else:
+        return False
     
+    
+while True:
+    request = input(">")
+    
+    if request=="exit":
+        break
+    if request.split()[0] in ["rmdir","rm"]:
+        if verify_delete(request.split()[1]):
+            pass
+        else:
+            break
+        
     sock = socket.socket()
     sock.connect((HOST, PORT))
     
@@ -16,4 +43,4 @@ while True:
     response = sock.recv(1024).decode()
     print(response)
     
-    sock.close()
+sock.close()
--- ftp-server.py
+++ ftp-server.py
@@ -1,52 +1,106 @@
 import socket
 import os
-'''
+import shutil
+"""
 pwd - показывает название рабочей директории
 ls - показывает содержимое текущей директории
 cat <filename> - отправляет содержимое файла
-mkdir <dir> - создаёт папку
-'''
+mkdir <path> - создаёт папку
+rmdir <path> - удаляет папку
+rm <filename> - удалить файл
+rename <old filename> <new filename> - переименовать файл
+copycs <filename from> <filename to>
+copysc <filename from> <filename to>
+exit (отключение) реализовано на клиенте
+"""
 
-userdir = os.path.join(os.getcwd(), 'docs')
-
-def check_access(dir):
-    pass
+userdir = os.path.join(os.getcwd(), "docs")
+    
+def check_access(path):
+    fullpath = str()
+    if os.path.isabs(path):
+        fullpath = path
+    else:
+        fullpath = os.path.join(userdir,path)
+    if userdir in fullpath:
+        return True
+    return False
 
 def pwd():
     return userdir
 
-def ls(dir):
-    return '; '.join(os.listdir(dir))
+def ls(path):
+    return "Содержимое папки:\n" + "; ".join(os.listdir(path))
 
-def cat(dir):
-    content=str()
-    with open(dir,'r') as f:
+def cat(filename):
+    fullpath = str()
+    content = str()
+    if os.path.isabs(filename):
+        fullpath = filename
+    else:
+        fullpath = os.path.join(userdir,filename)
+    with open(fullpath, "r") as f:
         for line in f:
             content+=line
-    return content
+    return "Содержимое файла " + fullpath + ":\n" + content
     
-def mkdir(dir):
-    if not os.path.exists(os.path.join(os.getcwd(),'docs',dir)):
-        os.mkdir(dir)
-    return ''
+def mkdir(path):
+    if check_access(path):
+        fullpath = os.path.join(userdir,path)
+        if not os.path.exists(fullpath):
+            os.mkdir(fullpath)
+            return "Создана папка " + fullpath
+    return "Ошибка"
+
+def rmdir(path):
+    if check_access(path):
+        fullpath = os.path.join(userdir,path)
+        if os.path.exists(fullpath):
+            shutil.rmtree(fullpath)
+            return  "Удалена папка " + fullpath
+
+def rm(filename):
+    if check_access(filename):
+        fullpath = os.path.join(userdir,filename)
+        if os.path.exists(fullpath):
+            os.remove(fullpath)
+            return "Удалён файл " + fullpath
+    
+def copycs(filename_from,filename_to):
+    pass
 
 def process(req):
-    if req == 'pwd':
-        pwd()
-    elif req == 'ls':
-        ls(userdir)
-    elif req.split()[0]=='cat':
-        cat(req.split()[1])
-    elif req.split()[0]=='mkdir':
-        mkdir(req.split()[1])
+    res= ""
+    if req == "pwd":
+        res = pwd()
+    elif req == "ls":
+        res = ls(userdir)
+    elif req.split()[0]=="cat":
+        res = cat(req.split()[1])
+    elif req.split()[0]=="mkdir":
+        res = mkdir(req.split()[1])
+    elif req.split()[0]=="rmdir":
+        res = rmdir(req.split()[1])
+    elif req.split()[0]=="rm":
+        res = rm(req.split()[1])
+    elif req.split()[0]=="copycs":
+        res = copycs(req.split()[1],req.split()[2])
     else:
-        return 'bad request'
+        res="bad request"
+    return res
 
+def log(message,file):
+    if os.path.exists(os.path.join(os.getcwd(),file)):
+        with open(file,"a") as f:
+            f.write(message+"\n")
+    else:
+        with open(file,"w+") as f:
+            f.write(message+"\n")
 
 PORT = 6666
 
 sock = socket.socket()
-sock.bind(('', PORT))
+sock.bind(("", PORT))
 sock.listen()
 print("Прослушиваем порт", PORT)
 
@@ -58,5 +112,6 @@ while True:
     
     response = process(request)
     conn.send(response.encode())
+    log(response,"log.txt")
 
-    conn.close()
+conn.close()
