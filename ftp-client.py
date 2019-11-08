ea5a26f8052a11d95b68205ba155970840ae4e10
Albert <albert5162@mail.ru>
2019-11-08 11:56:52 +0300

    FTP

--- ftp-client.py
+++ ftp-client.py
@@ -1,16 +1,31 @@
 import socket
 
 HOST = 'localhost'
-PORT = 6666
+PORT = 6662
 
 while True:
     request = input('>')
-    
+    req = request.split()
     sock = socket.socket()
     sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
+
+    if (req[0] == 'CtS'):
+        qw = req[0] + ' ' + req[1]
+        print(qw)
+        sock.send(qw.encode())
+        response = sock.recv(1024).decode()
+        if response == "OK":
+            f = open(req[2], 'rb')
+            l = f.read(1024)
+            while (l):
+                sock.send(l)
+                l = f.read(1024)
+            sock.send('END'.encode())
+        else:
+            print('Error')
+    else:
+        sock.send(request.encode())
+
     response = sock.recv(1024).decode()
     print(response)
     
--- ftp-server.py
+++ ftp-server.py
@@ -9,14 +9,68 @@ cat <filename> - отправляет содержимое файла
 dirname = os.path.join(os.getcwd(), 'docs')
 
 def process(req):
-    if req == 'pwd':
+    req_list = req.split()
+    if req_list[0] == 'pwd':
         return dirname
-    elif req == 'ls':
+    elif req_list[0] == 'ls':
         return '; '.join(os.listdir(dirname))
+    elif req_list[0] == "mkdir":
+        if len(req_list) < 2 or req_list[1] == "":
+            return 'bad request'
+        try:
+            new_path = os.path.join(dirname, req_list[1])
+            os.mkdir(new_path)
+            return 'added'
+        except:
+            return 'folder exists'
+
+    elif req_list[0] == "remove":
+        if len(req_list) < 2 or req_list[1] == "":
+            return 'bad request'
+        try:
+            new_path = os.path.join(dirname, req_list[1])
+            os.remove(new_path)
+            return 'removed'
+        except:
+            return 'error'
+
+    elif req_list[0] == "rmdir":
+        if len(req_list) < 2 or req_list[1] == "":
+            return 'bad request'
+        try:
+            new_path = os.path.join(dirname, req_list[1])
+            os.rmdir(new_path)
+            return 'removed'
+        except:
+            return 'error'
+
+    elif req_list[0] == "rename":
+        if len(req_list) < 3 or req_list[1] == "" or req_list[2] == "":
+            return 'bad request'
+        try:
+            old_path = os.path.join(dirname, req_list[1])
+            new_path = os.path.join(dirname, req_list[2])
+            os.rename(old_path, new_path)
+            return 'renamed'
+        except:
+            return 'error'
+    elif req_list[0] == "CtS":
+        new_path = os.path.join(dirname, req_list[1])
+        conn.send("OK".encode())
+        f = open(new_path, 'wb')
+        hash = conn.recv(1024)
+        while hash:
+            f.write(hash)
+            hash = conn.recv(1024)
+            if (hash.decode() == 'END'):
+                break
+        f.close()
+        return 'added'
+
     return 'bad request'
 
 
-PORT = 6666
+PORT = 6662
 
 sock = socket.socket()
 sock.bind(('', PORT))
