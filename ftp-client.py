e179fa2e73cdd722f2375aa8fbc57116ef0ab7fa
StepIgor <45178763+StepIgor@users.noreply.github.com>
2019-11-08 11:39:40 +0300

    mkdir is ready!

--- ftp-client.py
+++ ftp-client.py
@@ -1,7 +1,29 @@
 import socket, os
 
+'''
+	pwd - current directory
+	ls - list of files
+	cat - file content
+	exit - exit from client
+	clear or cls - clear console window
+'''
+
+
 HOST = 'localhost'
 PORT = 6666
+
+try:
+	os.system('clear')
+	os.system('cls')
+except:
+	pass
+print('*****************************************************************\n')
+print('*  FTP-EMULATOR КЛИЕНТ. РАБОТА С ФАЙЛАМИ НА УДАЛЕННОМ СЕРВЕРЕ.  *\n')
+print('*****************************************************************\n')
+print('*  ОБРАТИТЕ ВНИМАНИЕ НА ДВЕ УДОБНЫЕ КОМАНДЫ:                    *\n')
+print('*  clear/cls - очистить окно консоли, exit - выйти              *\n')
+print('*****************************************************************\n')
+print('\n\n\n')
 
 while True:
 	request = input('>')
--- ftp-server.py
+++ ftp-server.py
@@ -4,16 +4,40 @@ import os
 '''
 	pwd - current directory
 	ls - list of files
+	cat - file content
 '''
 
 dirname = os.path.join(os.getcwd(), 'docs')
 
 def process(req):
-	if req == 'pwd':
+	req = req + ' _eoc_'
+	if req.split(' ')[0] == 'pwd':																					#текущая директория
 		return dirname
-	elif req == 'ls':
+	
+	elif req.split(' ')[0] == 'ls':																					#список файлов
 		return '; '.join(os.listdir(dirname))
-	return 'bad request'
+	
+	elif req.split(' ')[0] == 'cat':																				#содержимое файла
+		if req.split(' ')[1] != '_eoc_':
+			if os.path.exists(os.path.join(dirname,req.split(' ')[1])):
+				with open(os.path.join(dirname,req.split(' ')[1]),'r') as f:
+					return f.read()
+			else:
+				return 'no such file'
+		else:
+			return 'empty filename'
+	
+	elif req.split(' ')[0] == 'mkdir':
+		if req.split(' ')[1] != '_eoc_':
+			if os.path.exists(os.path.join(dirname,req.split(' ')[1])):
+				return 'dir (file) already exists!'
+			else:
+				os.mkdir(os.path.join(dirname,req.split(' ')[1]),777)
+				return f'Dir is created!'
+		else:
+			return 'no filename'
+	
+	return 'bad request'																							#неправильный запрос
 
 
 PORT = 6666
