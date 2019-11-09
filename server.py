56b3dcced8440e05a44ac58af4d31a9d69f98b46
Андрей Баранюк <andrew-baranuk@yandex.ru>
2019-11-09 16:29:46 +0300

    Task 3, task 4

--- server.py
+++ server.py
@@ -30,6 +30,28 @@ def mkdir(req):
     except OSError:
         return "This directory already exists"
 
+def rmdir(req):
+    try:
+        os.rmdir(curr_dir+'/'+req)
+        return ''
+    except:
+        os.chdir(curr_dir+'/'+req)
+        for i in os.listdir(curr_dir+'/'+req):
+            try:
+                rmdir(req+'/'+i)
+            except:
+                os.remove(i)
+        os.chdir(curr_dir+'/..')
+        os.rmdir(curr_dir+'/'+req)
+        return ''
+
+def rm(req):
+    filename = req
+    try:
+        os.remove(curr_dir+'/'+filename)
+        return ''
+    except OSError:
+        return "This file does not exist"
 
 def process(req):
     if req == 'exit':
@@ -42,7 +64,10 @@ def process(req):
         return cat(req)
     elif req[:6] == 'mkdir ':
         return mkdir(req)
-
+    elif req[:6] == 'rmdir ':
+        return rmdir(req[6:])
+    elif req[:3] == 'rm ':
+        return rm(req[3:])
     else:
         return 'bad request'
 
