9151a7d05fe5228b2d39ee8f5c7e108b787eb082
natashagushan <natysya.gushan@gmail.com>
2019-11-12 00:45:05 +0300

    Laba

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,34 @@
-import socket
+#-*-coding:cp1251-*-
+from  socket import socket, AF_INET, SOCK_STREAM
 
-HOST = 'localhost'
-PORT = 6666
 
+def recv(conn):
+    header = conn.recv(5)  # сколько принимаем
+    # try:
+    header = int(header.decode('cp1251'))
+    message = conn.recv(header).decode('cp1251')
+    return message
+# except:
+#     print('Клиент отключился')
+
+
+def send(conn, message):
+    header = len(message)
+    full_message = f'{header:5}{message}'.encode('cp1251')
+    conn.send(full_message)
+
+
+sock = socket(AF_INET, SOCK_STREAM)
+port = int(input('port'))
+host = input('host')
+sock.connect(('localhost', port))
 while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+    msg = recv(sock)
+    print(msg)
+    result = input('>')
+    send(sock, result)
+
+
+
+
+
--- ftp-server.py
+++ ftp-server.py
@@ -1,5 +1,10 @@
-import socket
+#-*-coding:cp1251-*-
 import os
+import threading
+from  socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+from json import load, dump
+
 '''
 pwd - показывает название рабочей директории
 ls - показывает содержимое текущей директории
@@ -8,28 +13,69 @@ cat <filename> - отправляет содержимое файла
 
 dirname = os.path.join(os.getcwd(), 'docs')
 
-def process(req):
-    if req == 'pwd':
-        return dirname
-    elif req == 'ls':
-        return '; '.join(os.listdir(dirname))
-    return 'bad request'
 
 
-PORT = 6666
+host = input('Введите хост') or 'localhost'
+port = input('Введите порт') or '0'
+port = int(port)
 
-sock = socket.socket()
-sock.bind(('', PORT))
-sock.listen()
-print("Прослушиваем порт", PORT)
+
+def recv(conn):
+     header = conn.recv(5) #сколько принимаем
+     try:
+         header = int(header.decode('cp1251'))
+         message = conn.recv(header).decode('cp1251')
+         return message
+     except:
+         print('Клиент отключился')
+
+def send(conn, message):
+    header = len(message)
+    full_message = f'{header:5}{message}'.encode('cp1251')
+    conn.send(full_message)
+
+
+def handle_client(conn):
+    auth(conn)
+    while 1:
+        received_msg = recv(conn)
+        if not received_msg:
+            conn.close()
+            return
+        if received_msg == 'pwd':
+            send(conn, dirname)
+        elif received_msg == 'ls':
+            send(conn, '; '.join(os.listdir(dirname)))
+        elif received_msg.startswith('cat'):
+            file = received_msg.split()[-1]
+            with open(f'docs/{file}') as f:
+                send(conn,f.read())
+        else:
+            send(conn, received_msg)
+
+
+def auth(conn: socket):
+    send(conn, 'Введите логин')
+    login = recv(conn)
+    send(conn, 'Введите пароль')
+    password = recv(conn)
+    with open("auth.json", "r") as file:
+        a = load(file)
+    if login in a and a[login] == password:
+        send(conn,'Аутентификация прошла успешно!')
+        return login
+    else:
+        send(conn, 'Аутентификация не пройдена. Повторите.')
+
+
+
+
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.bind((host, port))
+sock.listen(10)
+print("Прослушиваем порт", sock.getsockname()[1])
 
 while True:
     conn, addr = sock.accept()
-    
-    request = conn.recv(1024).decode()
-    print(request)
-    
-    response = process(request)
-    conn.send(response.encode())
-
-conn.close()
+    threading.Thread(target = handle_client, args = [conn]).start()
