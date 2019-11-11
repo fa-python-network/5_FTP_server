b6f6bc37fb11c7b735c5941b944a9c2c2270f20f
StepIgor <igor.stepanov@difres.ru>
2019-11-11 20:35:54 +0300

    Базовые задачи выполнены!
    Есть защита от проникновения в родительские директории!

--- ftp-client.py
+++ ftp-client.py
@@ -1,11 +1,18 @@
 import socket, os
 
 '''
-	pwd - current directory
-	ls - list of files
-	cat - file content
-	exit - exit from client
-	clear or cls - clear console window
+	pwd 								- текущая директория
+	ls 									- список файлов
+	cat <filename> 						- содержимое файла
+	mkdir <dirname> 					- создать папку
+	touch <name> [content] 				- создать файл с содержимым content
+	rm <file or dir name>				- удалить файл или папку с вложениями
+	rename <f/d name> <f/d name>		- переименовать файл, папку с первым названием
+	cp <f/d name> <f/d name>			- копировать файл, папку с первым названием
+	cd <dirname>						- перейти в папку dirname
+	exit 								- закрыть клиент
+	clear 								- очистить окно консоли
+	help 								- открыть помощь
 '''
 
 
@@ -21,31 +28,47 @@ print('*****************************************************************\n')
 print('*  FTP-EMULATOR КЛИЕНТ. РАБОТА С ФАЙЛАМИ НА УДАЛЕННОМ СЕРВЕРЕ.  *\n')
 print('*****************************************************************\n')
 print('*  ОБРАТИТЕ ВНИМАНИЕ НА ДВЕ УДОБНЫЕ КОМАНДЫ:                    *\n')
-print('*  clear/cls - очистить окно консоли, exit - выйти              *\n')
+print('*  clear - очистить окно консоли, exit - выйти, help - помощь   *\n')
 print('*****************************************************************\n')
 print('\n\n\n')
 
 while True:
 	request = input('>')
-	
+
 	if request == 'exit':
 		print('Клиент закрыт')
 		break
-	
+
 	elif request == 'clear':
 		try:
 			os.system('clear')
 			os.system('cls')
 		except:
 			pass
-	
+
+	elif request == 'help':
+		print('''
+pwd - текущая директория
+ls  - список файлов
+cat <filename> - содержимое файла
+mkdir <dirname> - создать папку
+touch <name> [content] - создать файл с содержимым content
+rm <file or dir name> - удалить файл или папку с вложениями
+rename <f/d name> <f/d name> - переименовать файл, папку с первым названием
+cp <f/d name> <f/d name> - копировать файл, папку с первым названием
+cd <dirname> - перейти в папку dirname
+exit - закрыть клиент
+clear - очистить окно консоли
+help - открыть помощь
+''')
+
 	else:
 		sock = socket.socket()
 		sock.connect((HOST, PORT))
-		
+
 		sock.send(request.encode())
-		
+
 		response = sock.recv(1024).decode()
 		print(response)
-		
+
 		sock.close()
--- ftp-server.py
+++ ftp-server.py
@@ -1,42 +1,151 @@
 import socket
 import os
+import shutil
 
 '''
-	pwd - current directory
-	ls - list of files
-	cat - file content
+	pwd 								- текущая директория
+	ls 									- список файлов
+	cat <filename> 						- содержимое файла
+	mkdir <dirname> 					- создать папку
+	touch <name> [content] 				- создать файл с содержимым content
+	rm <file or dir name>				- удалить файл или папку с вложениями
+	rename <f/d name> <f/d name>		- переименовать файл, папку с первым названием
+	cp <f/d name> <f/d name>			- копировать файл, папку с первым названием
+	cd <dirname>						- перейти в папку dirname
 '''
 
 dirname = os.path.join(os.getcwd(), 'docs')
+dirlevel = 0
 
 def process(req):
-	req = req + ' _eoc_'
+	global dirname																									#текущая папка
+	global dirlevel																									#уровень в файловой системе, чтобы не вылезти из дозволенных границ
+	req = req + ' _eoc_'																							#флаг окончания команды
 	if req.split(' ')[0] == 'pwd':																					#текущая директория
 		return dirname
-	
+
 	elif req.split(' ')[0] == 'ls':																					#список файлов
 		return '; '.join(os.listdir(dirname))
-	
+
 	elif req.split(' ')[0] == 'cat':																				#содержимое файла
 		if req.split(' ')[1] != '_eoc_':
 			if os.path.exists(os.path.join(dirname,req.split(' ')[1])):
-				with open(os.path.join(dirname,req.split(' ')[1]),'r') as f:
-					return f.read()
+				if os.path.isfile(os.path.join(dirname,req.split(' ')[1])):
+					if '..' in req.split(' ')[1]:
+						return 'Not allowed using .. in complex path!'
+					with open(os.path.join(dirname,req.split(' ')[1]),'r',encoding='UTF-8') as f:
+						return f.read()
+				else:
+					return 'This is not a file!'
 			else:
 				return 'no such file'
 		else:
 			return 'empty filename'
-	
-	elif req.split(' ')[0] == 'mkdir':
+
+	elif req.split(' ')[0] == 'mkdir':																				#создание папки
 		if req.split(' ')[1] != '_eoc_':
 			if os.path.exists(os.path.join(dirname,req.split(' ')[1])):
 				return 'dir (file) already exists!'
 			else:
+				if '..' in req.split(' ')[1]:
+					return 'Not allowed using .. in complex path!'
 				os.mkdir(os.path.join(dirname,req.split(' ')[1]),777)
 				return f'Dir is created!'
 		else:
 			return 'no filename'
-	
+
+	elif req.split(' ')[0] == 'rm':																					#удаление файла или папки
+		if req.split(' ')[1] != '_eoc_':
+			if os.path.exists(os.path.join(dirname,req.split(' ')[1])):
+				if '..' in req.split(' ')[1]:
+					return 'Not allowed using .. in complex path!'
+				if os.path.isfile(os.path.join(dirname,req.split(' ')[1])):
+					os.remove(os.path.join(dirname,req.split(' ')[1]))
+				else:
+					shutil.rmtree(os.path.join(dirname,req.split(' ')[1]))
+				return 'file/directory deleted!'
+			else:
+				return 'no such file or directory!'
+		else:
+			return 'no filename or dirname'
+
+
+	elif req.split(' ')[0] == 'touch':																					#создать файл (пустой или с содержимым)
+		if req.split(' ')[1] != '_eoc_':
+			if not os.path.exists(os.path.join(dirname,req.split(' ')[1])):
+				if '..' in req.split(' ')[1]:
+					return 'Not allowed using .. in complex path!'
+				if req.split(' ')[2] == '_eoc_':
+					with open(os.path.join(dirname,req.split(' ')[1]),'w') as f:
+						f.write('');
+				else:
+					with open(os.path.join(dirname,req.split(' ')[1]),'w') as f:
+						f.write('');
+					with open(os.path.join(dirname,req.split(' ')[1]),'a') as f:
+						for word in range(2,len(req.split(' '))-1):
+							f.write(req.split(' ')[word]+' ');
+
+				return 'file created!'
+			else:
+				return 'File already exists!'
+		else:
+			return 'filename required!'
+
+	elif req.split(' ')[0] == 'rename':																					#переименовать файл или папку
+		if req.split(' ')[1] != '_eoc_' and req.split(' ')[2] != '_eoc_':
+			if os.path.exists(os.path.join(dirname,req.split(' ')[1])):
+				if '..' in req.split(' ')[1] or '..' in req.split(' ')[2]:
+					return 'Not allowed using .. in complex path!'
+				os.rename(os.path.join(dirname,req.split(' ')[1]),os.path.join(dirname,req.split(' ')[2]))
+				return 'rename done!'
+			else:
+				return 'First file/folder does not exist!'
+		else:
+			return 'filename (x2) required!'
+
+
+	elif req.split(' ')[0] == 'cp':																					#копировать файл или папку
+		if req.split(' ')[1] != '_eoc_' and req.split(' ')[2] != '_eoc_':
+			if os.path.exists(os.path.join(dirname,req.split(' ')[1])):
+				if '..' in req.split(' ')[1] or '..' in req.split(' ')[2]:
+					return 'Not allowed using .. in complex path!'
+				if os.path.isfile(os.path.join(dirname,req.split(' ')[1])):
+					shutil.copy(os.path.join(dirname,req.split(' ')[1]),os.path.join(dirname,req.split(' ')[2]))
+				else:
+					shutil.copytree(os.path.join(dirname,req.split(' ')[1]),os.path.join(dirname,req.split(' ')[2]))
+				return 'copy done!'
+			else:
+				return 'First file/folder does not exist!'
+
+
+
+	elif req.split(' ')[0] == 'cd':																		#сменить текущий каталог (с защитой файлов родительской директории)
+		if req.split(' ')[1] != '_eoc_':
+			if os.path.exists(os.path.join(dirname,req.split(' ')[1])):
+				if (os.path.isdir(os.path.join(dirname,req.split(' ')[1]))):
+					if '..' in req.split(' ')[1] and req.split(' ')[1] != '..':
+						return 'Not allowed using .. in complex path!'
+					if req.split(' ')[1] == '.':
+						return f'Now you are in {dirname}'
+					if req.split(' ')[1] == '..':
+						if dirlevel > 0:																#не находимся ли мы уже в корне пользовательской папки?
+							dirlevel-=1
+						else:
+							return 'Not allowed!'
+					dirname = os.path.join(dirname,req.split(' ')[1])
+					if req.split(' ')[1] != '..':														#спустились на один уровень вниз
+						dirlevel+=1
+					return f'Now you are in {dirname}'
+				else:
+					return 'This is not a dir!'
+			else:
+				return 'Dir with such name does not exist!'
+		else:
+			return 'dir name required!'
+
+
+
+
 	return 'bad request'																							#неправильный запрос
 
 
@@ -48,10 +157,10 @@ sock.listen(5)
 
 while True:
 	conn, addr = sock.accept()
-	
+
 	request = conn.recv(1024).decode()
 	print(request)
-	
+
 	response = process(request)
 	conn.send(response.encode())
 
