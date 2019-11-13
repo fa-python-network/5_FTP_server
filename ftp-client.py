8b578dddd02921456de2da1e6b4d04b9ccead832
AbdullinaDariya <abdullinadariya2711@gmail.com>
2019-11-13 10:55:25 +0300

    Основное задание 1-5

--- ftp-client.py
+++ ftp-client.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = 'localhost'
-PORT = 9098
+PORT = 9097
 
 while True:
     request = input('Чего желаете? > ')
--- ftp-server.py
+++ ftp-server.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+pwd-сервер вернет название рабочей директории
+ls-сервер вернет список файлов в рабочей директории
+cat <filename> сервер вернет сожержимое файла
+
+"""
 import socket
 import os
 import shutil
@@ -18,29 +25,29 @@ def process(req):
         return dirname
     elif req == 'ls':
         return '; '.join(os.listdir(dirname))
-    elif req== 'cat filik':
-        #file_name='filik.txt'
-        f=open(path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'filik.txt'))
-        line=f.readline()
-        spisok_strok=[]
-        while line:
-            spisok_strok.append(line)
-            line=f.readline()
-        f.close() 
-        return ' '.join(spisok_strok)
-    elif req == 'mkdir my_dir':
-        return os.mkdir('my_dir')
-    elif req == 'rmdir my_dir':
-        return shutil.rmtree('my_dir')
-    elif req == 'remove filik':
-        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'filik.txt')
+    elif 'cat'in req:
+        nazv=req.split()[1]
+        f=open(os.path.join(os.path.abspath(os.path.dirname(__file__)), nazv))
+        return f.read()
+    elif 'mkdir' in req:
+        nazv=req.split()[1]
+        return os.mkdir(nazv)
+    elif 'rmdir' in req:
+        nazv=req.split()[1]
+        return shutil.rmtree(nazv)
+    elif 'remove' in req:
+        nazv=req.split()[1]
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), nazv)
         return os.remove(path)
-    elif req=='rename filik':
-        return os.rename('filik.txt','my_file.txt')
+    elif 'rename' in req:
+        nazv_star=req.split()[1]
+        nazv_nov=req.split()[2]
+        return os.rename(nazv_star,nazv_nov)
     else:
         return 'Может Вы ошиблись?'
-        
-PORT = 9098
+
+  
+PORT = 9097
 sock = socket.socket()
 sock.bind(('', PORT))
 sock.listen()
@@ -52,11 +59,36 @@ while True:
     request = conn.recv(1024).decode()
     print(request)
     response = process(request)
-    if (request=='pwd') or (request=='ls') or (request=='cat filik'):
+    if (request == 'pwd') or (request == 'ls') or ('cat' in request):
         conn.send(response.encode())
-    elif (request=='mkdir my_dir') or (request == 'rmdir my_dir') or (request == 'remove filik') or (request == 'rename filik'):
+    elif ('mkdir' in request) or ('rmdir' in request) or ('remove' in request) or ('rename' in request):
         conn.send('Я сделал, как Вы просили.(Сервер)'.encode())
     else:
         conn.send(response.encode())
+        
+        
+#    with open('text_file.txt', 'rb') as fs:
+#        sock.send(b'BEGIN')
+#        while True:
+#            data = fs.read(1024)
+#            print('Sending data', data.decode('utf-8'))
+#            sock.send(data)
+#            print('Sent data', data.decode('utf-8'))
+#            if not data:
+#                print('Breaking from sending data')
+#                break
+#        sock.send(b'ENDED')
+#        fs.close()
+# 
+#    #Receive file
+#    print("Receiving..")
+#    with open('text_file.txt', 'wb') as fw:
+#        while True:
+#            data = sock.recv(1024)
+#            if not data:
+#                break
+#            fw.write(data)
+#        fw.close()
+
 conn.close()
 input()
