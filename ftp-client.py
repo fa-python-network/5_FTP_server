3cd271786fef24210179ce6196e20c768b491ee5
kirilzlatinski <55870691+kirilzlatinski@users.noreply.github.com>
2019-11-14 00:22:57 +0300

    Add files via upload

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,26 @@
 import socket
 
-HOST = 'localhost'
-PORT = 6666
 
+def receive(sock):
+    count = int(sock.recv(4).decode())
+    return sock.recv(count).decode()
+
+
+def send(sock, message):
+    message = message[:1020]
+    count = len(message)
+    sock.send('{:4}{}'.format(count, message).encode())
+
+
+host, port = input('Host:Port\n').split(':')
+port = int(port)
+sock = socket.socket()
+sock.connect((host, port))
 while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+    msg = receive(sock)
+    print(msg)
+    command = input('>')
+    if command == 'exit':
+        break
+    send(sock, command)
+sock.close()
--- ftp-server.py
+++ ftp-server.py
@@ -1,35 +1,73 @@
 import socket
 import os
+
 '''
 pwd - показывает название рабочей директории
 ls - показывает содержимое текущей директории
 cat <filename> - отправляет содержимое файла
 '''
 
-dirname = os.path.join(os.getcwd(), 'docs')
 
 def process(req):
-    if req == 'pwd':
-        return dirname
-    elif req == 'ls':
-        return '; '.join(os.listdir(dirname))
-    return 'bad request'
+    try:
+        if req == 'pwd':
+            return os.path.relpath(os.getcwd(), startdir)
+        elif req == 'ls':
+            return '; '.join(os.listdir())
+        elif 'cat' in req:
+            file = req.split()[-1]
+            with open(file) as f:
+                return f.read()
+        elif 'mkdir' in req:
+            dirname = req.split()[-1]
+            os.mkdir(dirname)
+            return 'Success'
+        elif 'chdir' in req:
+            dirname = req.split()[-1]
+            os.chdir(dirname)
+            return 'Success'
+        return 'bad request'
+    except Exception as e:
+        return e
 
 
-PORT = 6666
+def receive(conn):
+    count = int(conn.recv(4).decode())
+    return conn.recv(count).decode()
+
+
+def send(conn, message):
+    message = message[:1020]
+    count = len(message)
+    conn.send('{:4}{}'.format(count, message).encode())
+
+
+PORT = 6667
 
 sock = socket.socket()
 sock.bind(('', PORT))
 sock.listen()
 print("Прослушиваем порт", PORT)
+os.chdir('docs')
+startdir = os.getcwd()
 
-while True:
-    conn, addr = sock.accept()
-    
-    request = conn.recv(1024).decode()
-    print(request)
-    
-    response = process(request)
-    conn.send(response.encode())
-
-conn.close()
+try:
+    while True:
+        conn, addr = sock.accept()
+        send(conn, 'Welcome, what\'s your name?')
+        name = receive(conn)
+        try:
+            os.mkdir(name)
+        except:
+            pass
+        os.chdir(name)
+        send(conn, 'Welcome, {}'.format(name))
+        while True:
+            try:
+                req = receive(conn)
+                resp = process(req)
+                send(conn, resp)
+            except:
+                conn.close()
+finally:
+    sock.close()
