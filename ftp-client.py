56bef0c017e80d1998d26a5d7f3d540d6dc9bd7a
Sangadzhi <zaiasg@mail.ru>
2019-11-17 20:35:14 +0300

    main

--- ftp-client.py
+++ ftp-client.py
@@ -3,15 +3,35 @@ import socket
 HOST = 'localhost'
 PORT = 6666
 
+try:
+    os.system('clear')
+    os.system('cls')
+except:
+    pass
+
+
+print('Для выхода введите команду exit')
+print('Для справки введите команду help')
+
 while True:
     request = input('>')
-    
+    if request == 'exit':
+	    print('Клиент закрыт')
+	    break
+    elif request == 'help':
+        print('''pwd - показывает название рабочей директории
+ls - показывает содержимое текущей директории
+cat <filename> - отправляет содержимое файла
+mkdir <directoryname> - создает директорию
+rmdir <directoryname> - удаляет директорию
+remove <filename> - удаляет путь к файлу
+rename <filename> - переименовывает файл
+copy <old name> <new name> - копирует файл''')
+        
+
     sock = socket.socket()
     sock.connect((HOST, PORT))
-    
     sock.send(request.encode())
-    
     response = sock.recv(1024).decode()
     print(response)
-    
     sock.close()
