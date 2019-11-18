a17b8f56bff004f4252c19c964f413e206380eef
kruchinina <55450444+kruchinina@users.noreply.github.com>
2019-11-19 04:26:14 +0500

    Zadaniya

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,28 @@
-import socket
+from socket import socket, AF_INET, SOCK_STREAM
 
-HOST = 'localhost'
-PORT = 6666
 
+def recv(conn):
+    header = conn.recv(5)
+    header = int(header.decode())
+    message = conn.recv(header).decode('windows-1251')
+    return message
+
+
+def send(conn, message):
+    header = len(message)
+    full_message = f'{header:5}{message}'.encode('windows-1251')
+    conn.send(full_message)
+
+
+sock = socket(AF_INET, SOCK_STREAM)
+host = input('host: ')
+port = int(input('port: '))
+sock.connect(('localhost', port))
 while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+    msg = recv(sock)
+    print(msg)
+    request = input('')
+    send(sock, request)
+    if request == 'exit':
+        sock.close()
+        break
--- ftp-server.py
+++ ftp-server.py
@@ -1,35 +1,79 @@
-import socket
 import os
+from json import load
+from socket import AF_INET, SOCK_STREAM
+import threading
+import socket
+
+
 '''
 pwd - показывает название рабочей директории
 ls - показывает содержимое текущей директории
 cat <filename> - отправляет содержимое файла
 '''
+host = input('HOST: ') or 'localhost'
+port = input('PORT: ') or '0'
+port = int(port)
 
 dirname = os.path.join(os.getcwd(), 'docs')
 
-def process(req):
-    if req == 'pwd':
-        return dirname
-    elif req == 'ls':
-        return '; '.join(os.listdir(dirname))
-    return 'bad request'
+
+def recv(conn):
+    header = conn.recv(5)
+    try:
+        header = int(header.decode('windows-1251'))
+        message = conn.recv(header).decode('windows-1251')
+        return message
+    except:
+        print('Отключение клиента')
 
 
-PORT = 6666
+def send(conn, message):
+    header = len(message)
+    full_message = f'{header:5}{message}'.encode('windows-1251')
+    conn.send(full_message)
 
-sock = socket.socket()
-sock.bind(('', PORT))
-sock.listen()
-print("Прослушиваем порт", PORT)
+
+def auth(conn: socket):
+    send(conn, 'Введите логин')
+    login = recv(conn)
+    send(conn, 'Введите пароль')
+    password = recv(conn)
+    with open("База.json", "r") as file:
+        a = load(file)
+    if login in a and a[login] == password:
+        send(conn, 'OK')
+        return login
+    else:
+        send(conn, 'Неправильное имя пользователя или пароль')
+
+
+def handle_client(conn):
+    auth(conn)
+    while 1:
+        m = recv(conn)
+        if not m:
+            conn.close()
+            return
+        if m == 'pwd':
+            send(conn, dirname)
+
+        elif m == 'ls':
+            send(conn, '; '.join(os.listdir(dirname)))
+
+        elif m.startswith('cat'):
+            file = m.split()[-1]
+            with open(f'docs/{file}') as f:
+                send(conn, f.read())
+        else:
+            send(conn, m)
+
+
+
+sock = socket.socket(AF_INET, SOCK_STREAM)
+sock.bind((host, port))
+sock.listen(10)
+print("Прослушиваем порт", sock.getsockname()[1])
 
 while True:
     conn, addr = sock.accept()
-    
-    request = conn.recv(1024).decode()
-    print(request)
-    
-    response = process(request)
-    conn.send(response.encode())
-
-conn.close()
+    threading.Thread(target = handle_client, args = [conn]).start()
