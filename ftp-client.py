f8c8f0f35cf38216ad3d12f477b9cd3ebeb9659c
zaretskaya07 <55573397+zaretskaya07@users.noreply.github.com>
2019-11-19 08:59:12 +0300

    вроде не вылетает больше

--- ftp-client.py
+++ ftp-client.py
@@ -1,7 +1,8 @@
-import socket, os
+import socket
 from datetime import datetime
 
-
+sock = socket.socket()
+sock.setblocking(1)
 print("**************************")
 print("pwd - текущая директория")
 print("ls - содержимое текущей директории")
@@ -12,10 +13,28 @@ print("remove <name> - удалить файл")
 print("rename <old> <new> - изменить имя (почему то не работает :( )")
 print("exit - выход")
 print("**************************")
-HOST = 'localhost'
-PORT = 6666
-sock = socket.socket()
-sock.connect((HOST, PORT))
+
+while True:
+    HOST = input('Введите адрес хоста или localhost : \n')
+    if HOST == 'localhost':
+        HOST = '127.0.0.1'
+        break
+    host_l = HOST.split('.')
+    if (0 < int(host_l[0]) < 255) and (0 < int(host_l[1]) < 255) and (0 < int(host_l[2]) < 255) and (
+            0 < int(host_l[3]) < 255):
+        break
+    else:
+        print('Введен неверный формат адреса.')
+while True:
+    PORT = input('Введите номер порта от 1024 до 49151: \n')
+    if 1023 < int(PORT) < 49152:
+        break
+    else:
+        print('Неверный номер порта.')
+        break
+
+
+sock.connect((HOST, int(PORT)))
 while True:
     data = sock.recv(1024)
     print(data.decode())
@@ -36,24 +55,19 @@ while True:
     if "Добро пожаловать" in data.decode():
         break
 
-while True:
-    request = input('>')
+msg = ""
+while msg != 'exit':
+    msg = input('>')
     file = open('server.log', 'a')
     file.write(f"{datetime.now()} соединение установлено \n \n")
     file.close()
-    if request == 'exit':
+    if msg == 'exit':
         file=open('server.log','a')
         file.write(f"{datetime.now()} закрыто соединение \n \n")
         print('Клиент закрыт')
         file.close()
-        break
-
-    else:
-
-
-        sock.send(request.encode())
-
-        response = sock.recv(1024).decode()
-        print(response)
+    sock.send(msg.encode())
+    data = sock.recv(1024)
+    print(data.decode())
 
 sock.close()
--- ftp-server.py
+++ ftp-server.py
@@ -3,10 +3,10 @@ import os
 import csv
 from datetime import datetime
 
-
+sock = socket.socket()
 #dirname = os.path.join(os.getcwd(), 'docs')
 
-dirlevel = 0
+
 def process(req):
     file=open('server.log', 'a')
     if req.split(' ')[0] == 'pwd':  # текущая директория
@@ -79,12 +79,55 @@ def process(req):
     file.close()
     return 'bad request'  # неправильный запрос
 
+while True:
+    file = open('server.log', 'a')
+    host = input('Введите адрес хоста или localhost или пропустите этот шаг, нажав Enter: \n')
+    file.write("Запрашиваю адрес хоста...\n")
+    if host == 'localhost':
+        host = '127.0.0.1'
+        file.write(f"введенный адрес хоста: {host} \n")
+        file.close()
+        break
+    if host == '':
+        file.write("адрес хоста не установлен. \n")
+        file.close()
+        break
+    host_l = host.split('.')
+    if (0 < int(host_l[0]) < 255) and (0 < int(host_l[1]) < 255) and (0 < int(host_l[2]) < 255) and (
+            0 < int(host_l[3]) < 255):
+        file.write(f"Введенный адрес хоста: {host} \n")
+        file.close()
+        break
+    else:
+        print('Введен неверный формат адреса.')
+        file.write("Введен неверный адрес хоста, запрашиваю адрес снова.\n")
 
-PORT = 6666
+while True:
+    file = open('server.log', 'a')
+    port = input('Введите номер порта от 1024 до 49151: \n')
+    port = int(port)
+    file.write("Запрашиваю номер порта...\n")
+    if 1023 < port < 49152:
+        file.write(f"введенный номер порта: {port} \n")
+        file.close()
+        break
+    else:
+        print('Неверный номер порта.')
+        file.write("введен неверный номер порта, запрашиваю номер снова...\n")
 
-sock = socket.socket()
-sock.bind(('', PORT))
-sock.listen(5)
+while True:
+    try:
+        sock.bind(('', port))
+        break
+    except:
+        if 1023 < port <= 49150:
+            port += 1
+        else:
+            port = 9090
+
+sock.listen(1)
+print(f'Слушаю порт {port}.')
+
 
 while True:
     conn, addr = sock.accept()
@@ -158,11 +201,20 @@ while True:
         '''
         conn.send(answer.encode())
 
-
-    request = conn.recv(1024).decode()
-    print(request)
-
-    response = process(request)
-    conn.send(response.encode())
-
+    while True:
+        file = open('server.log', 'a')
+        msg = conn.recv(1024)
+        if msg.decode() == 'exit':
+            file.write(f"Попращался с {addr[0]}. \n \n \n")
+            conn.send(b"bye!")
+            file.close()
+            break
+        print(msg.decode())
+        response = process(msg.decode())
+        conn.send(response.encode())
+        file.write(f"Выполнил {msg} от {addr}. \n")
+        file.close()
     conn.close()
+
+
+
