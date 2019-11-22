9734ef73dc4931775b5dd4dfc29eec59fb0ced84
Alina <kl.ali18@ya.ru>
2019-11-22 22:26:19 +0300

    1

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,115 @@
 import socket
+import os
+import threading
+def increment_client_port(number_port):
+    return 1024 if number_port >= 65500 else number_port + 1
 
-HOST = 'localhost'
-PORT = 6666
+def check_input_host(host_address):
+    try:
+        if host_address == 'localhost':
+            return True
+        else:
+            host_address_list = host_address.split('.')
+            for i in host_address_list:
+                if int(i) <= 0 or int(i) > 255:
+                    return False
+            else:
+                return True
+    except ValueError:
+        return False
 
-while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
+
+def check_input_port(port_number, default_value=1556):
+    try:
+        port_number = int(port_number)
+        if 1024 > port_number or port_number >= 65535:
+            raise ValueError
+        print(f"Data is correct. Port is {port_number}")
+    except ValueError:
+        print(f"Data is not correct. Port is {default_value}")
+        port_number = default_value
+    return port_number
+
+
+if __name__ == "__main__":
+    HOST = input("Input host address IPv4 or localhost, DEFAULT - 127.0.0.1: ")
+
+    if check_input_host(HOST):
+        print(f"Data is correct. Host is {HOST}")
+    else:
+        print('Data is not correct. Host is 127.0.0.1')
+        HOST = '127.0.0.1'
+
+    PORT = input('Input port. Default - 1556: ')
+    PORT = check_input_port(PORT)
+
+    client_port = input('Input port of your connection. Default: 60272. ')
+    client_port = check_input_port(client_port, default_value=60272)
+    while True:
+        try:
+            sock = socket.socket()
+            sock.bind(('127.0.0.1', client_port))
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.connect((HOST, PORT))
+            break
+        except OSError:
+            sock.close()
+            client_port = increment_client_port(client_port)
+
+    while True:
+        resp = sock.recv(1024).decode()
+        print(resp)
+        if resp.startswith("Welcome"):
+            break
+        req = input()
+        sock.send(req.encode())
+
+    request = input(">> ")
+    if not request:
+        request = input(">> ")
+    elif request == "stop":
+        sock.send(request.encode())
+
+
+    while True:
+        if not request:
+            request = input(">> ")
+        elif request == "stop":
+            sock.send(request.encode())
+            break
+        else:
+            if request.startswith('sends'):
+                lst = list(request.split())
+                if len(lst) != 3:
+                    print("Incorrect args")
+                try:
+                    with open(os.path.join(os.path.join(os.getcwd(), 'client'), lst[1]), 'r', encoding='utf-8') as f1:
+                        inner = f1.read()
+                        string = 'sends ' + lst[2] +' '+ inner
+                    sock.send(string.encode())
+                except BrokenPipeError:
+                    pass
+            elif request.startswith('sendc'):
+                lst = list(request.split())
+                if len(lst) != 3:
+                    print("ncorrect args")
+                else:
+                    if lst[2] in os.listdir(os.path.join(os.getcwd(), 'client')):
+                        print("Cant rewrite file")
+                    else:
+                        sock.send(request.encode())
+            else:
+                sock.send(request.encode())
+            response = sock.recv(1024)
+            if response.decode().startswith('sendc'):
+                lst = list(response.decode().split(' ', 2))
+                try:
+                    with open(os.path.join(os.path.join(os.getcwd(), 'client'), lst[1]), 'w', encoding='utf-8') as f2:
+                        f2.write(' '.join(lst[2:]))
+                except BrokenPipeError:
+                    pass
+            else:
+                print(response.decode())
+            request = input(">> ")
+    print('Closed')
     sock.close()
--- ftp-server.py
+++ ftp-server.py
@@ -1,35 +1,245 @@
 import socket
 import os
-'''
-pwd - показывает название рабочей директории
-ls - показывает содержимое текущей директории
-cat <filename> - отправляет содержимое файла
-'''
+import shutil
+import logging
+import json
+import hashlib
+import binascii
+from threading import Thread
 
-dirname = os.path.join(os.getcwd(), 'docs')
+"""
+pwd - print working directory name
+ls - shows inner of working dir
+cat <filename> - shows inner of file
+mkdir <dir name> - make dir
+remdir <dir name> - delete dir with evrthng in it
+rm <filename> - delete file
+rename <filename> - rename file
+sends <filename1> <filename2> - send f1 to server as f2
+sendc <filename1> <filename2> - send f1 to client as f2
+cd <..>/<dir_name> - change dir <1 lvl down>/<1 lvl up>
+"""
 
-def process(req):
-    if req == 'pwd':
-        return dirname
-    elif req == 'ls':
-        return '; '.join(os.listdir(dirname))
-    return 'bad request'
+PORT = 1556
+hom_dir = os.path.join(os.getcwd(), 'docs')
+cur_dir = hom_dir
+
+def hash_password(password: str) -> str:
+
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
 
 
-PORT = 6666
+def verify_password(stored_password: str, provided_password: str) -> bool:
 
-sock = socket.socket()
-sock.bind(('', PORT))
-sock.listen()
-print("Прослушиваем порт", PORT)
+    salt = stored_password[:64]
+    stored_password = stored_password[64:]
+    pwdhash = hashlib.pbkdf2_hmac(
+        'sha512',
+        provided_password.encode('utf-8'),
+        salt.encode('ascii'),
+        100000
+    )
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return pwdhash == stored_password
 
-while True:
+
+def connection_with_auth(sock):
+
     conn, addr = sock.accept()
-    
-    request = conn.recv(1024).decode()
-    print(request)
-    
-    response = process(request)
-    conn.send(response.encode())
+    address = ':'.join([str(i) for i in addr])
+    if address in data_users['users']:
+        conn.send(f"Hello {data_users['users'][address]['name']}! Enter passw".encode())
+        while True:
+            data_password = conn.recv(1024).decode()
+            if not data_password:
+                conn.send(f"Incorrect passw".encode())
+            else:
+                if verify_password(data_users['users'][address]['password'], data_password):
+                    conn.send(f"Welcome".encode())
+                    break
+                else:
+                    conn.send(f"Incorrect passw".encode())
+            conn.send("Enter passw".encode())
+    else:
+        conn.send(f"Name:".encode())
+        data_name = conn.recv(1024).decode()
+        conn.send(f"Passw:".encode())
+        data_pass = conn.recv(1024).decode()
+        if not data_name or not data_pass:
+            conn.send(f"Incorrect".encode())
+            return None, None, None
+        data_users['users'][address] = {'name': data_name, 'password': hash_password(data_pass)}
+        with open('data_users.json', 'w') as file:
+            json.dump(data_users, file)
+        conn.send(f"Welcome {data_name}. Password added".encode())
+    # conn.send("Ok".encode())
+    return conn, addr, data_users['users'][address]['name']
+
+
+class ClientThread(Thread):
+    def __init__(self, conn, addr, name):
+        Thread.__init__(self)
+        self.conn = conn
+        self.addr = addr
+        self.ip = addr[0]
+        self.port = addr[1]
+        self.name = name
+
+        logger.info(f"Connect client {addr[0]}:{addr[1]}")
+
+    def run(self):
+
+        while True:
+            try:
+                data = self.conn.recv(1024).decode()
+                if data == 'stop' or not data:
+                    logger.info(f"Disconnect client {self.addr[0]}:{self.addr[1]}")
+                    self.conn.close()
+                    break
+                else:
+                    logger.info(f"From client {self.addr[0]}:{self.addr[1]} - {data}")
+                    response = self.process(data)
+                    logger.info(f"To client {self.addr[0]}:{self.addr[1]} - {response}")
+                    try:
+                        self.conn.send(response.encode())
+                    except BrokenPipeError:
+                        logger.info(f"Disconnect client {self.addr[0]}:{self.addr[1]}")
+                    # conn.send(data.upper())
+            except ConnectionResetError:
+                self.conn.close()
+
+    def process(self, req):
+        global cur_dir
+        global hom_dir
+        try:
+            bool_var = False
+            for i in ['pwd', 'ls', 'cat', 'mkdir', 'remdir', 'rm', 'rename', 'sends', 'sendc', 'cd']:
+                if req.startswith(i):
+                    bool_var = True
+                    break
+            assert bool_var, "Incorrect command"
+            if req == 'pwd':
+                return cur_dir
+
+            elif req == 'ls':
+                return '; '.join(os.listdir(cur_dir))
+
+            elif req[:3] == 'cat':
+                filename = req[4:]
+                if filename not in os.listdir(cur_dir):
+                    return "Dir doesnt exist"
+                else:
+                    with open(os.path.join(cur_dir, filename), 'r', encoding='utf-8') as f:
+                        inner = f.read()
+                        return inner
+
+            elif req[:5] == 'mkdir':
+                filename = req[6:]
+                if filename in os.listdir(cur_dir):
+                    return "Dir doesnt exist"
+                else:
+                    if filename.startswith(cur_dir):
+                        os.mkdir(os.path.join(cur_dir, filename))
+                        return 'Dir ' + req[6:] + ' created'
+                    else:
+                        return "Err"
+            elif req[:6] == 'remdir':
+                filename = req[7:]
+                if filename not in os.listdir(cur_dir):
+                    return "Dir doesnt exist"
+                else:
+                    shutil.rmtree(os.path.join(cur_dir, filename))
+                    return 'Dir ' + req[7:] + ' deleted'
+
+            elif req[:2] == 'rm':
+                filename = req[3:]
+                if filename not in os.listdir(cur_dir):
+                    return "File doesnt exist"
+                else:
+                    os.remove(os.path.join(cur_dir, filename))
+                    return 'File ' + req[3:] + ' deleted'
+
+            elif req[:6] == 'rename':
+                lst = list(req.split())
+                if len(lst) != 3:
+                    return "Incorrect args"
+                else:
+                    if lst[1] not in os.listdir(cur_dir):
+                        return "File doesnt exist"
+                    else:
+                        os.rename(os.path.join(cur_dir, lst[1]), os.path.join(cur_dir, lst[2]))
+                        return 'File ' + lst[1] + ' renamed to ' + lst[2]
+            elif req[:5] == 'sends':
+                lst = list(req.split(' ', 2))
+                if lst[1] in os.listdir(os.path.join(os.getcwd(), 'server')):
+                    return "Can't rewrite file"
+                else:
+                    with open(os.path.join(os.path.join(os.getcwd(), 'server'), lst[1]), 'w', encoding='utf-8') as f2:
+                        f2.write(' '.join(lst[2:]))
+                        return "File copied"
+            elif req[:5] == 'sendc':
+                lst = list(req.split())
+                with open(os.path.join(os.path.join(os.getcwd(), 'server'), lst[1]), 'r', encoding='utf-8') as f1:
+                    inner = f1.read()
+                    string = 'sendc ' + lst[2] + ' ' + inner
+                self.conn.send(string.encode())
+
+            elif req[:2] == 'cd':
+                if len(req)==2:
+                    return "Err"
+                elif req[3:5] == '..':
+                    parts = cur_dir.split("/")
+                    pathing = "/".join(parts[:-1])
+                    if pathing.startswith(hom_dir):
+                        os.chdir(pathing)
+                    else:
+                        return "Err"
+                else:
+                    pathing = cur_dir + '/' + req[3:]
+                    if pathing.startswith(hom_dir):
+                        os.chdir(pathing)
+                    else:
+                        return "Err"
+                cur_dir = os.getcwd()
+                return "Dir changed"
+
+        except AssertionError:
+            return "Incorrect command"
+
+try:
+    with open("data_users.json", "r") as read_file:
+        data_users = json.load(read_file)
+except FileNotFoundError:
+    with open("data_users.json", 'wt') as write_file:
+        data_users = {'users': {}}
+        json.dump(data_users, write_file)
+
+#  Создается и используется объект логгирования
+logger = logging.getLogger("serverLogger")
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler("server.log")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+# logger.info("Start client session")
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('', PORT))
+# sock.listen(5)
+# print("Listen port", PORT)
+# conn, addr = sock.accept()
+# logger.info(f"Connect client {addr[0]}:{addr[1]}")
+threads = []
+while True:
+    sock.listen()
+    clientsock, clientAddress, name = connection_with_auth(sock)
+    newthread = ClientThread(clientsock, clientAddress, name)
+    newthread.start()
 
 conn.close()
+sock.close()
