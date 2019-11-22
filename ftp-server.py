6909f8cf65aed9618008d1df3cac77b508a7b845
Ann <anna.raibiekas@mail.ru>
2019-11-22 20:42:50 +0300

    FTP server

--- ftp-server.py
+++ ftp-server.py
@@ -1,35 +1,86 @@
 import socket
 import os
-'''
-pwd - показывает название рабочей директории
-ls - показывает содержимое текущей директории
-cat <filename> - отправляет содержимое файла
-'''
 
-dirname = os.path.join(os.getcwd(), 'docs')
+"""
+pwd
+ls
+cat <filename>
 
+"""
+PORT = 8888
+curr_dir = os.path.join(os.getcwd(), 'docs')
+cur_dir = os.getcwd()
+os.chdir('docs')
 def process(req):
+    global curr_dir
     if req == 'pwd':
-        return dirname
+        return curr_dir
+
     elif req == 'ls':
-        return '; '.join(os.listdir(dirname))
-    return 'bad request'
+        return '; '.join(os.listdir(curr_dir))
 
+    elif req[:6] == "mkdir ":
+        os.mkdir(req[6:])
+        return f'You created directory "{req[6:]}"'
 
-PORT = 6666
+    elif req[:6] == "rmdir ":
+        os.rmdir(req[6:])
+        return f'You deleted directory "{req[6:]}"'
+
+    elif req[:3] == "cd ":
+        curr_dir = os.path.join(os.getcwd(), req[3:])
+        os.chdir(req[3:])
+        return("You are in " + req[3:])
+
+    elif req[:4] == "cat ":
+        filename = req[4:]
+        try:
+            File = open(filename)
+            content = File.read()
+            File.close()
+            return content
+        except FileNotFoundError:
+            return "File doesn't exist"
+
+    elif req[:5] == "echo ":
+        try:
+            if len(req.split(' >> ')) == 2:
+                new_st = req.split(' >> ')
+                new_st[1] = os.path.join(curr_dir, new_st[1])
+                file = open(new_st[1], 'a')
+                file.write('\n')
+                file.write(new_st[0])
+                file.close()
+                return f'added to {new_st[1]}'
+
+            elif len(req.split(' > ')) == 2:
+                new_st = req.split(' > ')
+                new_st[1] = os.path.join(curr_dir, new_st[1])
+                file = open(new_st[1], 'w')
+                file.write('\n')
+                file.write(new_st[0])
+                file.close()
+                return f'written in {new_st[1]}'
+            else:
+                return 'wrong format'
+        except FileNotFoundError:
+            return 'wrong format'
+    else:
+        return 'wrong command'
 
 sock = socket.socket()
 sock.bind(('', PORT))
-sock.listen()
-print("Прослушиваем порт", PORT)
+sock.listen(5)
 
 while True:
+    print("Listening", PORT)
+
     conn, addr = sock.accept()
-    
+
+
     request = conn.recv(1024).decode()
     print(request)
-    
+
     response = process(request)
     conn.send(response.encode())
-
-conn.close()
+    conn.close()
