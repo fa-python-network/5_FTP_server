1754594b1bfe007754be922d47fdaa3d9c98c71a
Alex <siuziumov@mail.ru>
2019-12-23 23:21:17 +0300

    work 5

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,40 @@
 import socket
 
-HOST = 'localhost'
-PORT = 6666
+sock = socket.socket()
+sock.setblocking(1)
+
+host = input("Введите адрес хоста или lh: ")
+if host == "lh":
+	host = 'localhost'
+else:
+	host_ls = host.split(".", 4)
+	for i in host_ls:
+		if 0 <= int(i) <= 255:
+			pass
+		else:
+			host = 'localhost'
+port = int(input("Введите адрес порта: "))
+if 1024 <= int(port) <= 65535:
+	pass
+
+sock.connect((host, port))
+print('Введите Ваше сообщение/запрос или "exit" для выхода: ')
+msg = ""
 
 while True:
-    request = input('>')
-    
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+	request = input('>')
+	if request == "exit" or request == "":
+		a = 1
+		break
+	sock.send(request.encode())
+	response = sock.recv(1024).decode()
+	msg += request + ' '
+	print (response)
+
+if (a == 1) and (msg == ''):
+	print('Disconnecting...')
+	sock.close()
+else:
+	print('Disconnecting...')
+	sock.close()
+print("Вы ввели: ", msg)
--- ftp-server.py
+++ ftp-server.py
@@ -1,35 +1,112 @@
 import socket
+import threading
+import logging
 import os
-'''
-pwd - показывает название рабочей директории
-ls - показывает содержимое текущей директории
-cat <filename> - отправляет содержимое файла
-'''
-
-dirname = os.path.join(os.getcwd(), 'docs')
-
-def process(req):
-    if req == 'pwd':
-        return dirname
-    elif req == 'ls':
-        return '; '.join(os.listdir(dirname))
-    return 'bad request'
-
-
-PORT = 6666
+import shutil
 
 sock = socket.socket()
-sock.bind(('', PORT))
-sock.listen()
-print("Прослушиваем порт", PORT)
+logging.basicConfig(filename = "exp.log", filemode = "w", level = logging.INFO)
 
+client_host_port = 1024
+while client_host_port != 65536:
+    try:
+        logging.info(f'Сервер пытается подключиться к порту: {client_host_port}')
+        sock.bind(('', client_host_port))
+        break
+    except:
+        logging.error(f'При подключении к порту {client_host_port} возникла ошибка')
+        client_host_port += 1
+
+sock.listen(100)
+
+print('Порт: ', client_host_port)
+logging.info(f'Подключено к порту: {client_host_port}\n')
+
+def lazy_output(addr, data):
+    print(f"Пользователь {addr} закончил сессию")
+    print("Пользовательский ввод:", data, f"Пользователь: {addr}")
+    logging.info(f"Пользователь {addr} заканчивает сессию\n")
+
+def process(req):
+
+    dirname = os.path.join(os.getcwd(), 'docs')
+    if req.strip() == 'pwd':
+        logging.info(dirname)
+        return dirname
+
+    elif req.strip() == 'ls':
+        logging.info('; '.join(os.listdir(dirname)))
+        return '; '.join(os.listdir(dirname))
+
+    elif req.strip()[:6] == 'mkfile':
+        user_new_file = os.path.join(dirname, req.strip()[7:])
+        if os.path.isfile(user_new_file) == True:
+            return "Файл уже существует"
+        else:
+            file = open(user_new_file, 'w')
+            file.close()
+            return "Файл успешно создан!"
+
+    elif req.strip()[:6] == 'remove':
+        user_file = os.path.join(dirname, req.strip()[7:])
+        if os.path.isfile(user_file) == True:
+            os.remove(user_file)
+            return "Файл успешно удалён"
+        else:
+            return "Файл не существует в текущей директории"
+
+    elif req.strip()[:5] == 'mkdir':
+        user_new_dir = os.path.join(dirname, req.strip()[6:])
+        if os.path.isdir(user_new_dir) == True:
+            return "Такая папка уже существует"
+        else:
+            os.mkdir(user_new_dir)
+            return "Папка успешно создана!"
+
+    elif req.strip()[:5] == 'rmdir':
+        user_dir = os.path.join(dirname, req.strip()[6:])
+        if os.path.isdir(user_dir) == True:
+            os.rmdir(user_dir)
+            return "Папка успешно удалена"
+        else:
+            return "Папка не существует"
+
+    elif req.strip()[:6] == 'rmtree':
+        user_dir = os.path.join(dirname, req.strip()[7:])
+        if os.path.isdir(user_dir) == True:
+            shutil.rmtree(user_dir)
+            return "Папка успешно удалена"
+        else:
+            return "Папка не существует"
+
+   
+    logging.info('Неправильный запрос\n')
+    return 'bad request'
+
+def user_input(conn, addr):
+    msg = ''
+    while True:
+        data = conn.recv(1024).decode()
+        logging.info(f"Пользовательский ввод: {data}. Пользователь: {addr}")
+        if not data:
+            lazy_output(addr, msg)
+            break
+        try:
+            response = process(data)
+            if response == 'bad request':
+                msg += data + ' '
+            conn.send(response.encode())
+        except:
+            pass
+
+count = 0
 while True:
+    count += 1
     conn, addr = sock.accept()
-    
-    request = conn.recv(1024).decode()
-    print(request)
-    
-    response = process(request)
-    conn.send(response.encode())
+    print(f"\nНовое подключение: {addr}\n")
+    logging.info(f"Новое подключение: {addr}")
+    Th = threading.Thread(target = user_input, args = (conn, addr))
+    logging.info(f"Поток №{count}\n")
+    Th.start()
 
 conn.close()
