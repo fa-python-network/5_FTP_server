bb34929ed5ff364861a91159086aab39a7b8f665
Lomovtseva Ekaterina <elomovtseva.2000@mail.ru>
2020-05-25 19:28:32 +0300

    ftp server

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,23 @@
+# -*- coding: utf-8 -*-
+
 import socket
-
 HOST = 'localhost'
-PORT = 6666
-
+PORT = 7576
+description = 'Вам доступны следующие функции файлового менеджера:\npwd - отобразить название рабочей папки\nls - отобразить содержимое рабочей папки\ncat - отобразить содержимое файла\nmkdir - создать папку\nrm - удалить файл\nrmdir - удалить папку\ncp - копировать файл или папку\nmkfile - создать файл\neditf - редактировать файл\nmv - переименовать файл или папку\n\nДля начала введте start'
+print(description)
+start = input()
+sock = socket.socket()
+sock.connect((HOST, PORT))
+sock.send(start.encode())
 while True:
+        
     request = input('>')
     
     sock = socket.socket()
     sock.connect((HOST, PORT))
-    
     sock.send(request.encode())
-    
     response = sock.recv(1024).decode()
+    
     print(response)
     
-    sock.close()
+sock.close()
--- ftp-server.py
+++ ftp-server.py
@@ -1,30 +1,143 @@
-import socket
-import os
+# -*- coding: utf-8 -*-
+import socket, os, shutil
 '''
-pwd - показывает название рабочей директории
-ls - показывает содержимое текущей директории
-cat <filename> - отправляет содержимое файла
+pwd - отобразить название рабочей папки
+ls - отобразить содержимое рабочей папки
+cat - отобразить содержимое файла
+mkdir - создать папку
+rm - удалить файл
+rmdir - удалить папку
+cp - копировать файл или папку
+mkfile - создать файл
+editf - редактировать файл
+mv - переименовать файл или папку
 '''
-
-dirname = os.path.join(os.getcwd(), 'docs')
-
-def process(req):
-    if req == 'pwd':
-        return dirname
-    elif req == 'ls':
-        return '; '.join(os.listdir(dirname))
-    return 'bad request'
-
-
-PORT = 6666
-
+def process(request):
+    global address
+    if request == 'pwd':
+        return 'localhost'+str(os.getcwd())[30:]
+    
+    
+    elif request == ('ls'):
+        print ("ls")
+        return '; '.join(os.listdir())
+    
+    
+    elif request[0:3] == 'cat':
+        if request[4:13] == 'localhost':
+            with open (address+request[13:], 'r') as f:
+                return f.read()
+        else:
+            return 'У вас нет доступа к этим файлам'
+        
+        
+    elif request[0:5] == 'mkdir':
+        if request[6:15] == 'localhost': 
+            try:
+                os.mkdir(address+request[15:])
+                return 'Выполнено: папка создана'
+            except OSError:
+                return 'Не выполнено: папка не создана'
+        else:
+            return 'У вас нет доступа к этим файлам'
+        
+        
+    elif request[0:5] == 'rmdir':
+        if request[6:15] == 'localhost': 
+            try:
+                os.rmdir(address+request[16:])
+                return 'Выполнено: папка удалена'
+            except OSError:
+                return 'Не выполнено: папка не удалена. Возможно, в ней есть файлы. Удалите файлы, затем удалите папку.'
+        else:
+            return 'У вас нет доступа к этим файлам'
+        
+        
+    elif request[0:2] == 'rm':
+        if request[3:12] == 'localhost':
+            try:
+                os.remove(address+request[13:])
+                return 'Выполнено: файл удален'
+            except:
+                return 'Не выполнено: файл не удален'
+        else:
+            return 'У вас нет доступа к этим файлам'
+        
+        
+        
+    elif request[0:2] == 'cp':
+        if request[3:12] == 'localhost' and request.rfind('localhost') != 3:
+            try:
+                shutil.copyfile(address+request[13:].split()[0], address+request[(request.rfind('localhost')+10):])
+                return 'Выполнено: файл скопирован'
+            except:
+                return 'Не выполнено: файл не скопирован'
+        else:
+            return 'У вас нет доступа к этим файлам'
+        
+        
+        
+    elif request[0:6] == 'mkfile':
+        print(address+request[16:])
+        try:
+            if request[7:16] == 'localhost':   
+                f = open(address+request[16:], 'w')
+                
+                return 'Выполнено: файл создан'
+            else:
+                return 'У вас нет доступа к этим файлам'
+        except FileNotFoundError:
+            return 'Не выполнено: файл или директория не найдены'
+        
+        
+    elif request[0:5] == 'editf':
+        if request[6:15] == 'localhost':
+            try:
+                with open(address+request[15:].split()[0], 'a') as f:
+                    f.write(' '.join(address+request[15:].split()[1:]))
+                    return 'Выполнено: данные записаны в файл'
+            except:
+                with open(address+request[15:].split()[0], 'w') as f:
+                    f.write(' '.join(request[15:].split()[1:]))
+                    return 'Выполнено: файл создан и данные записаны'
+        else:
+            return 'У вас нет доступа к этим файлам'
+        
+        
+    elif request[0:2] == 'mv':
+        if request[3:12] == 'localhost' && request.rfind('localhost') != 3:
+            try:
+                shutil.move(address+request[13:].split()[0], address+request[(request.rfind('localhost')+10):])
+                return 'Выполнено: файл перемещен или переименован'
+            except:
+                return 'Не выполнено: файл не перемещен или не переименован'
+        else:
+            return 'У вас нет доступа к этим файлам'
+        
+        
+    else:
+        return 'server'
+    
+PORT = 7576
 sock = socket.socket()
 sock.bind(('', PORT))
 sock.listen()
-print("Прослушиваем порт", PORT)
-
+conn, addr = sock.accept()
+for i in range(9999):
+    try:
+        os.mkdir('C:\\Users\\user\\Desktop\\ftp\\'+str(addr[0])+'.'+str(i))
+        os.chdir('C:\\Users\\user\\Desktop\\ftp\\'+str(addr[0]+'.'+str(i)))
+        address = 'C:\\Users\\user\\Desktop\\ftp\\'
+        break
+    except FileExistsError:
+        pass
+request = conn.recv(1024).decode()
+response = process('Добро пожаловать!')
 while True:
+    print("Прослушивается порт " + str(PORT))
+    
     conn, addr = sock.accept()
+    print(addr)
     
     request = conn.recv(1024).decode()
     print(request)
@@ -32,4 +145,5 @@ while True:
     response = process(request)
     conn.send(response.encode())
 
-conn.close()
+sock.close()
+input()
