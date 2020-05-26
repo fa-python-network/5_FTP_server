19a3ec00235f5eaf070af2c97794fa93a58ecaac
“dronio328” <dronio328@gmail.com>
2020-05-26 16:26:21 +0300

    Абу А.К. ЗБ-ПИ-2-1

--- ftp-client.py
+++ ftp-client.py
@@ -1,17 +1,71 @@
 import socket
+import os
 
 HOST = 'localhost'
 PORT = 6666
 
+
+print('\nДля получения списка команд введите команду help')
+print('Для выхода введите команду exit\n')
+
 while True:
     request = input('>')
+    if request == 'exit':
+        print('Клиент закрыт')
+        break
     
-    sock = socket.socket()
-    sock.connect((HOST, PORT))
-    
-    sock.send(request.encode())
-    
-    response = sock.recv(1024).decode()
-    print(response)
-    
-    sock.close()
+    elif request == 'help':
+        print('''
+pwd - показывает название рабочей директории
+ls - содержимое текущей директории
+cat <Название папки> - отправляет содержимое файла
+mkdir <Название папки> - создает новую папку
+rmdir <Название папки> - удаляет папку
+create <Название файла> - создает файл
+remove <Название файла> - удаляет файл
+rename <Название файла> - переименовывает файл
+copy <Название файла> <Название нового файла> - копирует файл
+get <Название файла> - скачать с сервера файл
+push <Название файла> - загрузить на сервер файл
+exit - закрыть клиент
+help - помощь
+''')
+    else:
+        sock = socket.socket()
+        sock.connect((HOST, PORT))
+        if request.split(' ')[0] == 'push':
+            request+=' _eoc_'
+            if request.split(' ')[1] != '_eoc_':
+                if '..' in request.split(' ')[1] or '/' in request.split(' ')[1] or '\\' in request.split(' ')[1]:
+                    print('Не допускается использование .. / \\')
+                else:
+                    if os.path.exists(request.split(' ')[1]):
+                        if os.path.isfile(request.split(' ')[1]):
+                            sock.send(request.encode())
+                            with open(request.split(' ')[1],'r') as f:
+                                forsend = f.read().encode()
+                            sock.send(forsend)
+                            answer = sock.recv(1024)
+                            print(answer.decode())
+                        else:
+                            print('Это не файл')
+                    else:
+                        print('Такого файла нет')
+            else:
+                print('Вы забыли ввести название файла')
+
+        else:
+            sock.send(request.encode())
+            response = sock.recv(1024).decode()
+            if request.split(' ')[0] == 'get':
+                if response.split('-!-!-!->')[0] == 'success':
+                    with open(request.split(' ')[1],'w') as f:
+                        f.write(response.split('-!-!-!->')[1])
+                    print('Готово')
+                else:
+                    print(response)
+            else:
+                print(response)
+        
+        
+        sock.close()
--- ftp-server.py
+++ ftp-server.py
@@ -1,28 +1,96 @@
 import socket
 import os
+import shutil
+
 '''
 pwd - показывает название рабочей директории
 ls - показывает содержимое текущей директории
 cat <filename> - отправляет содержимое файла
+mkdir <Название папки> - создает новую папку
+rmdir <Название папки> - удаляет папку
+create <Название файла> - создает файл
+remove <Название файла> - удаляет файл
+rename <Название файла> - переименовывает файл
+copy <Название файла> <Название нового файла> - копирует файл
+get <Название файла> - скачать с сервера файл
+push <Название файла>   - загрузить на сервер файл
 '''
 
 dirname = os.path.join(os.getcwd(), 'docs')
-
 def process(req):
+    global conn
     if req == 'pwd':
         return dirname
     elif req == 'ls':
         return '; '.join(os.listdir(dirname))
+    elif req[:3] == 'cat':
+        path = os.path.join(os.getcwd(), 'docs', req[4::])
+        if os.path.exists(path):
+            with open(path, 'r+') as f:
+                line = ''
+                for l in f:
+                    line+=l
+            return line
+        else:
+            return 'Такого файла не существет'
+    elif req[:5] == 'mkdir':
+        path = os.path.join(os.getcwd(), 'docs', req[6::])
+        if not os.path.exists(path):
+            os.makedirs(path)
+            return f'Папка создана'
+        else: 
+            return 'Такая папка уже существет'
+    elif req[:5] == 'rmdir':
+        path = os.path.join(os.getcwd(), 'docs', req[6::])
+        if os.path.exists(path):
+            shutil.rmtree(os.path.join(os.getcwd(), 'docs', req[6::]))
+            return f'Папка удалена'
+        else:
+            return 'Такой папки не существует'
+    elif req[:6]  == 'create':
+        open(os.path.join(os.getcwd(), 'docs', req[7:]), 'tw', encoding='utf-8').close()
+        return f'Файл создан'
+    elif req[:6]  == 'remove':
+        os.remove(os.path.join(os.getcwd(), 'docs', req[7:]))
+        return f'Файл удален'
+    elif req[:6]  == 'rename':
+        req = req.split(' ')
+        os.rename(os.path.join(os.getcwd(), 'docs', req[1]), os.path.join(os.getcwd(), 'docs', req[2]))
+        return 'Файл переименован'
+    elif req[:4]  == 'copy':
+        req = req.split(' ')
+        shutil.copyfile(os.path.join(os.getcwd(), 'docs', req[1]), os.path.join(os.getcwd(), 'docs', req[2]))
+        return 'Файл скопирован'
+    elif req.split(' ')[0] == 'get':                                                                                #скачать текстовый файл
+        if req.split(' ')[1] != '_eoc_':
+            if os.path.exists(os.path.join(dirname,req.split(' ')[1])):
+                if os.path.isfile(os.path.join(dirname,req.split(' ')[1])):
+                    if '..' in req.split(' ')[1]:
+                        return 'Не допускается использование ..'
+                    with open(os.path.join(dirname,req.split(' ')[1]),'r',encoding='UTF-8') as f:
+                        return 'success-!-!-!->'+f.read()
+                else:
+                    return 'Это не файл'
+            else:
+                return 'Нет такого файла'
+        else:
+            return 'Не введено название файла'
+    elif req.split(' ')[0] == 'push':                                                                               #загрузить текстовый файл от клиента
+        if req.split(' ')[1] != '_eoc_':
+            if '..' in req.split(' ')[1] or '/' in req.split(' ')[1] or '\\' in req.split(' ')[1]:
+                return 'Не допускается использование .. / \\'
+            filecontent = conn.recv(1024)
+            with open(os.path.join(dirname,req.split(' ')[1]),'w') as f:
+                f.write(filecontent.decode())
+            return 'Готово'
     return 'bad request'
-
 
 PORT = 6666
 
 sock = socket.socket()
 sock.bind(('', PORT))
 sock.listen()
-print("Прослушиваем порт", PORT)
-
+print("Прослушиваем порт: ", PORT)
 while True:
     conn, addr = sock.accept()
     
@@ -31,5 +99,4 @@ while True:
     
     response = process(request)
     conn.send(response.encode())
-
 conn.close()
